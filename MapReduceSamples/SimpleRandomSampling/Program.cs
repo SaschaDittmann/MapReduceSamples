4b9bfaeb47e2c9fb3f3f3c7001ec4dbe88af3509
Sascha Dittmann <Sascha5812@gmx.de>
2014-06-18 09:19:24 +0000

    Simple Random Sampling sample added

---
+++ MapReduceSamples/SimpleRandomSampling/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Hadoop.MapReduce;
+
+namespace SimpleRandomSampling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hadoop = Hadoop.Connect();
+            var config = new HadoopJobConfiguration
+            {
+                InputPath = "/data/posts",
+                OutputFolder = "output/SimpleRandomSampling",
+            };
+            hadoop.MapReduceJob.Execute<SimpleRandomSamplingMapper>(config);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MapReduceSamples/SimpleRandomSampling/SimpleRandomSamplingMapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Hadoop.MapReduce;
+
+namespace SimpleRandomSampling
+{
+    public class SimpleRandomSamplingMapper : MapperBase
+    {
+        private readonly Random _rands = new Random();
+        private const double SamplingRate = 0.1;
+
+        public override void Map(string inputLine, MapperContext context)
+        {
+            if (_rands.NextDouble() < SamplingRate)
+                context.EmitLine(inputLine);
+        }
+    }
+}
