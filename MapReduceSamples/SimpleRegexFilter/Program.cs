f40e8fc627ed44d5e9bf6168a394283dee11f5ce
Sascha Dittmann <Sascha5812@gmx.de>
2014-06-18 09:08:24 +0000

    Simple Regex Filter Sample added

---
+++ MapReduceSamples/SimpleRegexFilter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Hadoop.MapReduce;
+
+namespace SimpleRegexFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hadoop = Hadoop.Connect();
+            var config = new HadoopJobConfiguration
+            {
+                InputPath = "/data/posts",
+                OutputFolder = "output/SimpleRegexFilter",
+            };
+            hadoop.MapReduceJob.Execute<RegexMapper>(config);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MapReduceSamples/SimpleRegexFilter/RegexMapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Hadoop.MapReduce;
+
+namespace SimpleRegexFilter
+{
+    public class RegexMapper : MapperBase
+    {
+        private readonly Regex _regex = new Regex("hdinsight|hadoop", RegexOptions.IgnoreCase);
+
+        public override void Map(string inputLine, MapperContext context)
+        {
+            if (_regex.IsMatch(inputLine))
+                context.EmitLine(inputLine);
+        }
+    }
+}
